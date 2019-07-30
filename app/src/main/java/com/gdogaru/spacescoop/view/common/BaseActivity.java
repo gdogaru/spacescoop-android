639442548845a764fff65cdbf2ad6a27356c0d48
Gabriel Dogaru <gdogaru@gmail.com>
2019-07-30 11:13:57 +0300

    copyright update

--- app/src/main/java/com/gdogaru/spacescoop/view/common/BaseActivity.java
+++ app/src/main/java/com/gdogaru/spacescoop/view/common/BaseActivity.java
@@ -1,6 +1,22 @@
-package com.gdogaru.spacescoop.view.common;
+/*
+ * Copyright (c) 2019 Gabriel Dogaru - gdogaru@gmail.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-import android.content.Context;
+package com.gdogaru.spacescoop.view.common;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -13,10 +29,9 @@ import javax.inject.Inject;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 /**
- * Created by Gabriel on 7/6/2015.
+ * @author Gabriel Dogaru (gdogaru@gmail.com)
  */
 public abstract class BaseActivity extends AppCompatActivity implements HasSupportFragmentInjector {
 
@@ -45,10 +60,4 @@ public abstract class BaseActivity extends AppCompatActivity implements HasSuppo
         bus.unregister(this);
         super.onPause();
     }
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
-
 }
